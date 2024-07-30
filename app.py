dc8efdc4a45bde93589124f23f0f382f8c361be8
astilegal <astilegal@gmail.com>
2024-07-30 22:29:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
 import os
+from flask import Flask, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
@@ -10,13 +10,17 @@ class ViewCount(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     count = db.Column(db.Integer, nullable=False, default=0)
 
-@app.before_first_request
-def create_tables():
+# Function to initialize the database
+def initialize_database():
     db.create_all()
     if ViewCount.query.count() == 0:
         initial_view = ViewCount(count=0)
         db.session.add(initial_view)
         db.session.commit()
+
+@app.before_request
+def before_request():
+    initialize_database()
 
 @app.route('/increment', methods=['POST'])
 def increment():
